e0f4cd3d5957743c20f8b3794ae5b7d3b73ec425
Fraaaaaa4 <87281326+fraaaaa4@users.noreply.github.com>
2023-10-20 19:50:53 -0400

    October 21st Version
    Settings Update

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace PrologParsec
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new MDIParent1());
+                
+            }
+            catch (ArgumentException ex)
+            {
+                
+            }
+
+
+        }
+    }
+}
